c9a8211b914ba23fb68b924a9a90b4315877b3f5
vinamra1102 <143907433+vinamra1102@users.noreply.github.com>
2026-05-30 21:16:38 +0000

    feat(data): add education

---
+++ src/data/education.ts
@@ -0,0 +1,17 @@
+export const education = [
+  {
+    school: "Manipal University Jaipur",
+    degree: "B.Tech, Information Technology",
+    year: "2022 — Present",
+  },
+  {
+    school: "Holy Cross Sr. Sec. School",
+    degree: "CBSE Class XII",
+    year: "2021",
+  },
+  {
+    school: "Holy Cross Sr. Sec. School",
+    degree: "CBSE Class X",
+    year: "2019",
+  },
+];
